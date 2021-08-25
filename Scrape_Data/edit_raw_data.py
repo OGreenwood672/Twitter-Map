d9d2feb77f445e35beef7600d9fbc0fb7c9ec1bd
Oliver Greenwood <oliver.greenwood@challoners.org>
2021-08-25 23:58:52 +0100

    Commented edit_raw_data.py

--- Scrape_Data/edit_raw_data.py
+++ Scrape_Data/edit_raw_data.py
@@ -1,14 +1,30 @@
 import json
+from typing import Dict, List
 
-def setMinFollowers(data, followers):
+def set_min_followers(data: Dict[List, List], min_followers: int) -> Dict[List, List]:
+    """ Function set_min_followers
+
+    @params
+    data: Entire dataset of Twitter Map
+    followers: minimum amount of followers to survive the PURGE
+
+    Function removes all nodes and links which don't match required min followers
+    """
     
-    removedIds = [user["id"] for user in data["nodes"] if user["followers"] < followers]
-    data["nodes"] = list(filter(lambda x: x["followers"] >= followers, data["nodes"]))
+    removedIds = [user["id"] for user in data["nodes"] if user["followers"] < min_followers]
+    data["nodes"] = list(filter(lambda x: x["followers"] >= min_followers, data["nodes"]))
     data["links"] = list(filter(lambda x: not (x["source"] in removedIds or x["target"] in removedIds), data["links"]))
 
     return data
 
-def removeSolos(data):
+def remove_solos(data: Dict[List, List]) -> List[Dict]:
+    """ Function remove_solos
+
+    @params
+    data: Entire dataset of Twitter Map
+
+    Function removes nodes which don't share any links with anyone else
+    """
 
     sources = list(map(lambda x: x["source"], data["links"]))
     targets = list(map(lambda x: x["target"], data["links"]))
@@ -17,17 +33,32 @@ def removeSolos(data):
     return data["nodes"]
 
 
-def shrink(links, scale):
+def shrink(links: List[Dict], chance_of_removal: int) -> List[Dict]:
+    """Function shrink
+    
+    @params
+    links: Entire set of links from the dataset
+    chance_of_removal: Inversly proportional to chance of removal TODO Need better name (also change name of line 71 (same name issue))
+
+    For large set of data, this function removes links to reduce lag
+    """
     
     new_links = []
     for index, link in enumerate(links):
-        if index % scale == 0:
+        if index % chance_of_removal == 0:
             new_links.append(link)
     
     return new_links
 
 
-def edit(nameOfFolder):
+def edit(nameOfFolder: str):
+    """ Function edit
+    
+    @params
+    nameOfFolder: folder to save the map in
+
+    This function allows the user to select edit options.
+    """
 
     with open(f"./public/Maps/{nameOfFolder}/TwitterMapRaw.json", "r") as f:
         data = json.load(f)
@@ -36,15 +67,17 @@ def edit(nameOfFolder):
 
     if "y" in input("    Would you like to shrink number of links (recommended for graphs above 750 nodes) (y/N)"):
         print("    Shrinking nodes to fit screen...")
-        data["links"] = shrink(data["links"], 8)
+        while not (chance_of_removal := input("  What is your chance of removal 1/x: ").isdigit()):
+            print("    That is not a valid number")
+        data["links"] = shrink(data["links"], chance_of_removal)
     
     if "y" in input("    Would you like to set a minimum followers for nodes (y/N)"):
-        while not (minFollowers := input("  What is your minimum followers required for node on map: ").isdigit()):
+        while not (min_followers := input("  What is your minimum followers required for node on map: ").isdigit()):
             print("    That is not a valid number")
-        data = setMinFollowers(data, minFollowers / 1000000)
+        data = set_min_followers(data, min_followers / 1000000)
     
     if "y" in input("   Would you like to remove the nodes who don't connect with anyone (y/N)"):
-        data["nodes"] = removeSolos(data)
+        data["nodes"] = remove_solos(data)
 
     with open(f"./public/{nameOfFolder}/TwitterMap.json", "w") as f:
         json.dump(data, f, indent=4)
