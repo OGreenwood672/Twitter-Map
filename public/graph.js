8c75e662989b692a1846e3cd312074169697be4b
Oliver Greenwood <oliver.greenwood@challoners.org>
2021-08-25 18:13:05 +0100

    Added cmd line argument to select map

--- public/graph.js
+++ public/graph.js
@@ -1,38 +1,59 @@
-url = "./Maps/ElonMusk/TwitterMap.json"
-fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (TwitterMap) {
-        const Graph = ForceGraph()
-        (document.getElementById('graph'))
-            .backgroundColor('#101020')
-            .nodeAutoColorBy('followers')
-            .linkColor(() => '#484848')
-            .nodeCanvasObject((node, ctx, globalScale) => {
-                const label = node.name;
-                const fontSize = 12/globalScale * (0.75 + node.followers/100*1.5);
-                ctx.font = `${fontSize}px Sans-Serif`;
-                const textWidth = ctx.measureText(label).width;
-                const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
-      
-                ctx.textAlign = 'center';
-                ctx.textBaseline = 'middle';
-                ctx.fillStyle = node.color;
-                ctx.fillText(label, node.x, node.y);
-      
-                node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
-              })
-              .nodePointerAreaPaint((node, color, ctx) => {
-                ctx.fillStyle = color;
-                const bckgDimensions = node.__bckgDimensions;
-                bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
-              })
-              .graphData(TwitterMap);
-        Graph.d3Force('charge').strength(-600);
-
-    })
-    .catch(function (err) {
-        console.log('error: ' + err);
+function getUrlVars() {
+    var vars = {};
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
     });
+    return vars;
+}
 
+function showMap(url) {
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (TwitterMap) {
+            const Graph = ForceGraph()
+            (document.getElementById('graph'))
+                .backgroundColor('#101020')
+                .nodeAutoColorBy('followers')
+                .linkColor(() => '#484848')
+                .nodeCanvasObject((node, ctx, globalScale) => {
+                    const label = node.name;
+                    const fontSize = 12/globalScale * (0.75 + node.followers/100*1.5);
+                    ctx.font = `${fontSize}px Sans-Serif`;
+                    const textWidth = ctx.measureText(label).width;
+                    const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+        
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'middle';
+                    ctx.fillStyle = node.color;
+                    ctx.fillText(label, node.x, node.y);
+        
+                    node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+                })
+                .nodePointerAreaPaint((node, color, ctx) => {
+                    ctx.fillStyle = color;
+                    const bckgDimensions = node.__bckgDimensions;
+                    bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+                })
+                .graphData(TwitterMap);
+            Graph.d3Force('charge').strength(-300);
+
+        })
+        .catch(function (err) {
+            console.log('Error: ' + err);
+        });
+
+
+}
+
+var urlArgs = getUrlVars();
+
+if (urlArgs["map"] === undefined) {
+    throw Error("No map has been requested")
+}
+
+var url = `./Maps/${urlArgs["map"]}/TwitterMap.json`;
+
+showMap(url)
--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 var express = require("express"); 
 var app = express(); 
-var path = require("path"); 
+var path = require("path");
+var fs = require("fs")
+
+const PORT = 6720;
+
+if (process.argv.length === 2) {
+
+    throw Error("No map argument provided")
+
+}
+
+var map_name = process.argv[process.argv.length-1];
+
 app.use(express.static('./public'))
-app.get('/',function(req,res){ 
-  res.sendFile(path.join(__dirname+'./public/index.html')); 
-  //__dirname : It will resolve to your project folder. 
-}); 
-app.listen(3000); 
-console.log("Server running at Port 3000"); 
+
+fs.readdir("./public/Maps", (err, files) => {
+
+    if (err) { throw err; }
+
+    if (files.indexOf(map_name) > -1) {
+
+        app.get('/',function(req,res){ 
+            res.sendFile(path.join(__dirname+'./public/index.html')); 
+        }); 
+
+        app.listen(PORT);
+        console.log(`Showing ${map_name} at:\nhttp://localhost:${PORT}/?map=${map_name}`)
+
+    } else {
+
+        console.log("No map called " + map_name);
+
+    }
+
+})
